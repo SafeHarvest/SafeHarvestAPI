1644cf4ac00885404abc45e2401c116ede2a7d9f
Jerry D'Antonio <stumpjumper@gmail.com>
2015-04-11 10:35:47 -0400

    Attempting to allow cross-site requests.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,12 @@ require 'faker'
 
 class WelcomeController < ApplicationController
 
+  before_filter :set_access
+
+  def set_access
+    @response.headers['Access-Control-Allow-Origin'] = '*'
+  end
+
   # GET /welcome
   def index
     render :json => {'message' => "#{Faker::Company.catch_phrase} to #{Faker::Company.bs}."}
